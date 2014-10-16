757d7446b0cd039ee3387890d4a5059710449bc8
thesupremecommander <thesupremecmdr@gmail.com>
2014-10-16 12:33:44 -0400

    Actually add the source and header files.

---
+++ src/externalextensions.cpp
@@ -0,0 +1,51 @@
+/*
+ *  externalextensions.cpp
+ *  ExternalExtensions project
+ *
+ *  Copyright (c) 2014 thesupremecommander
+ *  MIT License
+ *  http://opensource.org/licenses/MIT
+ *
+ */
+
+#include "externalextensions.h"
+
+WebSockets *g_WebSockets = nullptr;
+
+//
+// The plugin is a static singleton that is exported as an interface
+//
+ExternalExtensionsPlugin g_ExternalExtensionsPlugin;
+EXPOSE_SINGLE_INTERFACE_GLOBALVAR(ExternalExtensionsPlugin, IServerPluginCallbacks, INTERFACEVERSION_ISERVERPLUGINCALLBACKS, g_ExternalExtensionsPlugin );
+
+ExternalExtensionsPlugin::ExternalExtensionsPlugin() {}
+ExternalExtensionsPlugin::~ExternalExtensionsPlugin() {}
+
+bool ExternalExtensionsPlugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceFn gameServerFactory) {
+	g_WebSockets = new WebSockets();
+
+	std::thread ws(&WebSockets::Run, g_WebSockets);
+	ws.detach();
+
+	return true;
+}
+
+void ExternalExtensionsPlugin::Unload(void) {}
+void ExternalExtensionsPlugin::Pause(void) {}
+void ExternalExtensionsPlugin::UnPause(void) {}
+const char *ExternalExtensionsPlugin::GetPluginDescription(void) { return PLUGIN_DESC; }
+void ExternalExtensionsPlugin::LevelInit(char const *pMapName) {}
+void ExternalExtensionsPlugin::ServerActivate(edict_t *pEdictList, int edictCount, int clientMax) {}
+void ExternalExtensionsPlugin::GameFrame(bool simulating) {}
+void ExternalExtensionsPlugin::LevelShutdown(void) {}
+void ExternalExtensionsPlugin::ClientActive(edict_t *pEntity) {}
+void ExternalExtensionsPlugin::ClientDisconnect(edict_t *pEntity) {}
+void ExternalExtensionsPlugin::ClientPutInServer(edict_t *pEntity, char const *playername) {}
+void ExternalExtensionsPlugin::SetCommandClient(int index) {}
+void ExternalExtensionsPlugin::ClientSettingsChanged(edict_t *pEdict) {}
+PLUGIN_RESULT ExternalExtensionsPlugin::ClientConnect(bool *bAllowConnect, edict_t *pEntity, const char *pszName, const char *pszAddress, char *reject, int maxrejectlen) { return PLUGIN_CONTINUE; }
+PLUGIN_RESULT ExternalExtensionsPlugin::ClientCommand(edict_t *pEntity, const CCommand &args) { return PLUGIN_CONTINUE; }
+PLUGIN_RESULT ExternalExtensionsPlugin::NetworkIDValidated(const char *pszUserName, const char *pszNetworkID) { return PLUGIN_CONTINUE; }
+void ExternalExtensionsPlugin::OnQueryCvarValueFinished(QueryCvarCookie_t iCookie, edict_t *pPlayerEntity, EQueryCvarValueStatus eStatus, const char *pCvarName, const char *pCvarValue) {}
+void ExternalExtensionsPlugin::OnEdictAllocated(edict_t *edict) {}
+void ExternalExtensionsPlugin::OnEdictFreed(const edict_t *edict) {}
---
+++ src/websockets.h
@@ -0,0 +1,70 @@
+/*
+ *  websockets.h
+ *  ExternalExtensions project
+ *
+ *  Copyright (c) 2014 thesupremecommander
+ *  MIT License
+ *  http://opensource.org/licenses/MIT
+ *
+ */
+
+#pragma once
+
+#define _WEBSOCKETPP_CPP11_THREAD_
+#define _WEBSOCKETPP_CPP11_FUNCTIONAL_
+#define _WEBSOCKETPP_CPP11_SYSTEM_ERROR_
+#define _WEBSOCKETPP_CPP11_RANDOM_DEVICE_
+#define _WEBSOCKETPP_CPP11_MEMORY_
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include <websocketpp/common/thread.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <json/json.h>
+
+#include "dbg.h"
+
+class WebSockets {
+public:
+	void Run();
+	boost::uuids::uuid RegisterConnectHook(std::function<void(websocketpp::connection_hdl)> function);
+	boost::uuids::uuid RegisterDisconnectHook(std::function<void(websocketpp::connection_hdl)> function);
+	boost::uuids::uuid RegisterMessageHook(std::function<void(websocketpp::connection_hdl, Json::Value)> function);
+	void UnregisterConnectHook(boost::uuids::uuid id);
+	void UnregisterDisconnectHook(boost::uuids::uuid id);
+	void UnregisterMessageHook(boost::uuids::uuid id);
+	void SendPrivateMessage(websocketpp::connection_hdl recipient, Json::Value message);
+	void SendGlobalMessage(Json::Value message);
+
+	typedef enum {
+		ActionType_Connected,
+		ActionType_Disconnected,
+		ActionType_IncomingMessage,
+		ActionType_OutgoingPrivateMessage,
+		ActionType_OutgoingGlobalMessage
+	} ActionType;
+
+	typedef struct {
+		ActionType type;
+		websocketpp::connection_hdl associatedConnection;
+		Json::Value message;
+	} Action;
+
+private:
+	void OnOpen(websocketpp::connection_hdl connection);
+	void OnClose(websocketpp::connection_hdl connection);
+	void OnMessage(websocketpp::connection_hdl connection, websocketpp::server<websocketpp::config::asio>::message_ptr message);
+	void ProcessEvents();
+
+	websocketpp::server<websocketpp::config::asio> server;
+	std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> currentConnections;
+	std::queue<Action> actions;
+	boost::uuids::random_generator uuidGenerator;
+	std::map<boost::uuids::uuid, std::function<void(websocketpp::connection_hdl)>> connectHooks;
+	std::map<boost::uuids::uuid, std::function<void(websocketpp::connection_hdl)>> disconnectHooks;
+	std::map<boost::uuids::uuid, std::function<void(websocketpp::connection_hdl, Json::Value)>> messageHooks;
+	websocketpp::lib::mutex actionLock;
+	websocketpp::lib::condition_variable actionNotification;
+	websocketpp::lib::mutex connectionLock;
+};
